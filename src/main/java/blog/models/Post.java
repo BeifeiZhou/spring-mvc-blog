aae2eb9a69babbe03996c587257cd0f423e66c9b
beifei.zhou <z.beifei@gmail.com>
2016-09-01 18:03:42 +0100

    post, user, postservice

--- src/main/java/blog/models/Post.java
+++ src/main/java/blog/models/Post.java
@@ -1,7 +1,60 @@
 package blog.models;
+import java.util.Date;
 
 /**
  * Created by bzhou on 01/09/16.
  */
 public class Post {
+    private Long id;
+    private String title;
+    private String body;
+    private User author;
+    private Date date = new Date();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString(){
+        return "User{" + "id=" + id + ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", date='" + date + '\'' + '}';
+    }
 }
--- src/main/java/blog/models/PostService.java
+++ src/main/java/blog/models/PostService.java
@@ -1,7 +1,16 @@
 package blog.models;
+import blog.models.Post;
+import java.util.List;
+
 
 /**
  * Created by bzhou on 01/09/16.
  */
-public class PostService {
+public interface PostService {
+    List<Post> findAll();
+    List<Post> findLatest5();
+    Post findById(Long id);
+    Post create(Post post);
+    Post edit(Post post);
+    void deleteById(Long id);
 }
